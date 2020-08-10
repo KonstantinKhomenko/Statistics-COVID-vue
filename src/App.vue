64c99e2f00a8007a77e62c908c6f6acee2779541
KonstantinKhomenko <kostya5599@gmail.com>
2020-08-10 14:00:31 +0300

    drawer

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" class="deep-orange lighten-3" dark app>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.link">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="deep-orange darken-1" dense dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="headline">
+        <v-icon class="mr-2">mdi-virus</v-icon>COVID-19
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Country {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    items: [
+      { title: 'World summary', icon: 'mdi-earth', link: '/' },
+      { title: 'Countries', icon: 'mdi-flag', link: '/countries' },
+      { title: 'News', icon: 'mdi-newspaper', link: '/' }
+    ],
+    drawer: false
+  })
+};
+</script>
